f7867e0cd88239824dceeb66344ddbee93a1adc7
Alondra Maldonado <alondra.mal96@gmail.com>
2022-01-05 23:03:08 -0800

    fix create button

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,74 +1,3 @@
-// const Flight = require('../models/flight'); 
-// const Ticket = require('../models/ticket'); 
-
-// module.exports = {
-// 	index,
-// 	new: newFlight,
-//   create,
-// 	show,
-//   newTicket,
-// }
-// function index(req, res) {
-//     Flight.find({}, function (err, flightDocuments) {
-//       res.render("flights/index", {
-//         title: "Flights",
-//         flights: flightDocuments,
-//       });
-//     });
-//   }
-
-//   function newFlight(req, res){
-//       const newFlight = new Flight();
-//     //   obtain the default date
-//     const dt = newFlight.departs;
-//     // format the date for the value attribute of the input
-//     // timezone offset of default date
-//     let timezoneOffset =dt.getTimezoneOffset() * 60000;
-//     // subtract offset from the default date
-//     const departureDate = new Date(dt - timezoneOffset).toISOString();
-//     // render local departure date
-//       res.render("flights/new", {departureDate});
-//       console.log(departureDate)
-
-//   }
-
-//   function create(req, res){
-//     Flight.create(req.body, function (err, flightDocument) {
-//         res.redirect("/flights");
-//       });
-//   }
-
-//   function show(req, res){
-//     // console.log(req.params, " < -req.params in the show route")
-//     const newFlight = new Flight();
-//     //   obtain the default date
-//     const dt = newFlight.departs;
-//     // format the date for the value attribute of the input
-//     // timezone offset of default date
-//     let timezoneOffset =dt.getTimezoneOffset() * 60000;
-//     // subtract offset from the default date
-//     const departureDate = new Date(dt - timezoneOffset).toISOString();
-//     // render local departure date
-//     Flight.findById(req.params.id, function(err, flight) {
-//       Ticket.find({ flight: flight._id }, function(err, tickets) {
-//         res.render('flights/show', {
-//           flight,
-//           tickets,
-//           departureDate,
-//           title: "flight details"
-//         });
-//       });
-//     });
-//   }
-
-//   function newTicket(req, res) {
-//     // res.send("new ticket function envoked");
-//     Flight.findById(req.params.id, function (err, flight) {
-//       console.error(err);
-//       res.render("tickets/new", { flight });
-//     });
-//   }
-
 const Flight = require("../models/flight");
 const Ticket = require("../models/ticket");
 
@@ -129,6 +58,7 @@ function show(req, res) {
   Flight.findById(req.params.id, function (err, flight) {
     Ticket.find({ flight: flight._id }, function (err, tickets) {
       res.render("flights/show", {
+
         flight,
         tickets
       });
--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,19 +1,28 @@
 const Ticket = require('../models/ticket');
 const Flight = require('../models/flight');
+const flight = require('../models/flight');
 
 module.exports = {
-	new: newTicket,
-	create,
+  new: newTicket,
+  create,
 };
 
-function newTicket(req, res) {
-	res.render('tickets/new', { flightId: req.params.id });
-}
 
 function create(req, res) {
-	flightId = req.params.id;
-	req.body.flight = flightId;
-	Ticket.create(req.body, function(err, ticket) {
-		res.redirect(`/flights/${flightId}`);
-	});
+   
+  Flight.findById(req.params.id, function (err, flightDocument) {
+    req.body.flight = flightDocument._id
+  Ticket.create(req.body, function (err, ticket) {
+    
+  console.log("ticket function is working")
+    res.redirect(`/flights/${flightDocument._id}`);
+  })
+})
+}
+
+function newTicket(req, res) {
+  console.log("This is the newTicket Function, which is called when you select add tickets ")
+    res.render('tickets/new', {
+     
+  })
 }
